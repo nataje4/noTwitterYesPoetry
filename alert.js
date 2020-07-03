6b1406da609a057afe5d7b50c6108d78f06de1ea
Natalie Jane Edson <nataliejaneedson@gmail.com>
2020-07-03 09:33:39 -0700

    refines things, makes you enter a password

--- alert.js
+++ alert.js
@@ -1,1 +1,33 @@
-alert("Are you sure you actually want to be on Twitter right now?"); 
+function makePasscode() {return Math.random().toString(36).substr(2, 10)} 
+
+
+function initialPrompt() {
+	passcode = makePasscode()
+	promptText = "Are you sure you actually want to be on twitter right now? If so, please type this passcode into the input box: \n\n" + passcode
+	userInput = prompt(promptText, ""); 
+
+	if (userInput == null) {
+		window.location.href = "https://poets.org/poem-a-day"
+	} else if (userInput == passcode) {
+		return null;
+	} else {
+		errorPrompt();
+	}
+}
+
+
+function errorPrompt() {
+	passcode = makePasscode()
+	promptText = "Looks like you entered the wrong passcode. Try again with this one: \n\n" + passcode
+	userInput = prompt(promptText, ""); 
+
+	if (userInput == null) {
+		window.location.href = "https://poets.org/poem-a-day"
+	} else if (userInput == passcode) {
+		return null;
+	} else {
+		errorPrompt();
+	}
+}
+
+initialPrompt();
